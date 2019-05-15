375ea3027e45c53a60632ea062b71246bd2f52a6
Sanna Ketomaki <sanna.ketomaki@stud.pointcollege.fi>
2019-05-15 20:03:00 +0300

    Luotu controllerit ja suunniteltu View:t. Ei vielä muuta koodia

---
+++ AlytaloMvc/AlytaloMvc/Controllers/TaloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlytaloMvc.Controllers
+{
+    public class TaloController : Controller
+    {
+        // GET: Talo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AlytaloMvc/AlytaloMvc/Controllers/ValoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlytaloMvc.Controllers
+{
+    public class ValoController : Controller
+    {
+        // GET: Valo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
